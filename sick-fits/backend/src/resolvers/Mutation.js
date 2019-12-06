d1c4177a93b3e307c6e7460d57286aea4c5c1a7d
Will Cook <hello@doubleucook.com>
2019-12-07 08:35:31 +1300

    playing around with queries and mutations in graphql yoga

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {};
+
+
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    console.log('args: ', args)
+
+    // create a new dog
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
 module.exports = Mutations;
