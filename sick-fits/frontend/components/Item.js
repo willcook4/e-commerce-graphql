69c78b2c29ef1a51c4184a75b21cd79bd9284226
Will Cook <hello@doubleucook.com>
2019-12-09 16:14:21 +1300

    init all items query setup to get the items on the sell page via apollo and to render the result

---
+++ sick-fits/frontend/components/Item.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+import Title from './styles/Title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
+
+class Item extends Component {  
+  render() {
+    const { item } = this.props;
+    return (
+      <ItemStyles>
+        <Title>
+          <Link href={{
+            pathname: '/item',
+            query: { id: item.id }
+          }}>
+            <a>{item.title}</a>
+          </Link>
+        </Title>
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
+        <p>{item.description}</p>
+
+        <div className='buttonList'>
+          <Link href={{
+            pathname: 'update',
+            query: { id: item.id }
+          }}>
+            <a>Edit ✏️</a>
+          </Link>
+          <button>Add to Cart</button>
+          <button>Delete</button>
+        </div>
+      </ItemStyles>
+    );
+  }
+}
+
+Item.propTypes = {
+  item: PropTypes.object.isRequired
+};
+
+export default Item;
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,6 +1,8 @@
+import Items from '../components/Items';
+
 const Home = (props) => (
   <div>
-    <p>Hey!</p>
+    <Items />
   </div>
 )
 
