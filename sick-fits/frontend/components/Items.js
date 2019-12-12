f33e0dcbf1c76840f60f199c2dd3251232549e58
Will Cook <hello@doubleucook.com>
2019-12-12 14:47:45 +1300

    pagination working except for cache invalidation

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import { perPage } from '../config';
 
 import Item from './Item';
 import Pagination from './Pagination';
@@ -19,8 +20,12 @@ const ItemsList = styled.div`
 `
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(
+      first: $first,
+      skip: $skip,
+      orderBy: createdAt_DESC
+    ) {
       id
       title
       price
@@ -36,7 +41,12 @@ class items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page}></Pagination>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: this.props.page * perPage - perPage,
+            // first: perPage
+          }}>
           {({data, error, loading}) => {
             if(loading) return (<p>Loading...</p>)
             if(error) return (<p>Error: {error.message}</p>)
