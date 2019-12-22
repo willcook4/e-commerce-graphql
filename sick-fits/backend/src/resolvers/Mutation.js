bb906e798c96c52dbd66ddf04ca16e13277c8ba0
Will Cook <hello@doubleucook.com>
2019-12-23 12:15:39 +1300

    permissions update on the backend and save to the db on change on /permissions

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { transport, makeANiceEmail } = require('../mail');
+const { hasPermission } = require('../utils');
 
 const Mutations = {
   // createDog(parent, args, ctx, info) {
@@ -185,6 +186,36 @@ const Mutations = {
 
     // Return the user
     return updatedUser
+  },
+
+  async updatePermissions(parent, args, ctx, info) {
+    // check if the user is logged in
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in!')
+    }
+    // query the current user
+    const currentUser = await ctx.db.query.user({
+      where: {
+        id: ctx.request.userId
+      }
+    }, info)
+
+    // check if they have permissions to do this action
+    hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']);
+
+    // update the permissions
+    return ctx.db.mutation.updateUser(
+      {
+        data: {
+          permissions: {
+            set: args.permissions // user set due to enum
+          }
+        },
+        where: {
+          id: args.userId
+        }
+      }, info
+    );
   }
 };
 
