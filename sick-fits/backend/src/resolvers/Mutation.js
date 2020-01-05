20fc13d448462df0088ae51e7116c45188ec0f7f
Will Cook <hello@doubleucook.com>
2020-01-06 08:57:39 +1300

    added permissions checking and basic front end error handling for deleteItem

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -56,9 +56,16 @@ const Mutations = {
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     // find the item
-    const item = await ctx.db.query.item({ where }, `{id title}`);
+    const item = await ctx.db.query.item({ where }, `{id title user { id }}`);
     // check if they own the item or have permissions to delete this item
-    // TODO
+    const ownsItem = item.user.id === ctx.request.userId
+    const hasPermissions = ctx.request.user.permissions.some(permission => ['ADMIN', 'ITEMDELETE'].includes(permission))
+    console.log('ownsItem?', item.user.id, ctx.request.userId)
+    console.log('hasPermissions? ', hasPermissions)
+    if(!ownsItem && hasPermissions) {
+      throw new Error('You don\'t have permission to do that!');
+    }
+    
     // delete the item
     return ctx.db.mutation.deleteItem({ where }, info);
   },
--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -38,7 +38,9 @@ class DeleteItem extends Component {
             <button 
               onClick={() => {
                 if( confirm('Are you sure you want to delete this?') ) {
-                  deleteItem();
+                  deleteItem().catch(err => {
+                    alert(err.message)
+                  });
                 }
               }}>{this.props.children}</button>
           )}
