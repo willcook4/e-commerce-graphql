df65fc0a04e3b054457d5776e81aec0fff2ea236
Will Cook <hello@doubleucook.com>
2020-01-21 10:53:03 +1300

    initial item shallow rendering testing

---
+++ sick-fits/frontend/__tests__/item.test.js
@@ -0,0 +1,48 @@
+import ItemComponent from '../components/Item';
+import { shallow } from 'enzyme';
+
+const fakeItem = {
+  id: 'ABCD123',
+  title: 'A cool item',
+  price: 5000,
+  description: 'This is a really cool item',
+  image: 'dog.jpg',
+  largeImage: 'largedog.jpg'
+}
+
+describe('<Item/>', () => {
+  it('renders the image properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />)
+    // console.log(wrapper.debug()) 
+    const img = wrapper.find('img')
+    // console.log(img.props())
+    expect(img.props().src).toBe(fakeItem.image)
+    expect(img.props().alt).toBe(fakeItem.title)
+  })
+  
+  it('renders the price tag and title properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />)
+    // console.log(wrapper.debug())    
+    const priceTag = wrapper.find('PriceTag')
+    // console.log(PriceTag.debug())
+    expect(priceTag.children().text()).toBe('$50')
+
+    const title = wrapper.find('Title a')
+    // console.log(Title.debug())
+    expect(title.text()).toBe(fakeItem.title)
+  })
+
+  it('renders out the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />)
+    // console.log(wrapper.debug())
+    const buttonList = wrapper.find('.buttonList')
+    console.log(buttonList.debug())
+    expect(buttonList.children()).toHaveLength(3)
+    // console.log(buttonList.debug())
+    expect(buttonList.find('Link')).toHaveLength(1)
+    expect(buttonList.find('Link').exists()).toBe(true)
+
+    expect(buttonList.find('AddToCart').exists()).toBe(true)
+    expect(buttonList.find('DeleteItem').exists()).toBe(true)
+  })
+})
